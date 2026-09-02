7cf80fc8537ff992feac136afd622ab2261f5711
Alpha114-crypto <alphaluka94@gmail.com>
2026-09-02 13:37:06 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const authRoutes = require('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
 const courseRoutes = require('./routes/courseRoutes');
+const academicRoutes = require('./routes/academicRoutes');
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.get('/api/health', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/courses', courseRoutes);
+app.use('/api/academics', academicRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
